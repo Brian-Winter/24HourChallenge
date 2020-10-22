c6249947eb55434872e3a7fe546ca56dd6718c29
Brian-Winter <pawndesigns@gmail.com>
2020-10-22 00:00:06 -0400

    Added Comment-Reply-Like Entities

---
+++ NerdyNetwork.Data/Reply.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdyNetwork.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int CommentReplyId{ get; set; }
+
+        [ForeignKey(nameof(Comments))]
+        public int CommentId { get; set; }
+        public Comments Comment { get; set; }
+        // public int MyProperty { get; set; } replyComment
+    }
+}
