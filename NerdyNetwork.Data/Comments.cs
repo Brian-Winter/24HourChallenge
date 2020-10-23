ea15e04e980ee7f83ba0908a85807a0b6b4d08ea
Brian-Winter <pawndesigns@gmail.com>
2020-10-22 20:04:02 -0400

    attempt merge

---
+++ NerdyNetwork.Data/Comments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdyNetwork.Data
+{
+    public class Comments
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User Author { get; set; }
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public Post CommentPost { get; set; }
+    }
